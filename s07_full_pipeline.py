b4899ee85cdb2f92d1d6c334b379e8734ebf168e
sedajorgensencareer <sedajorgensen2003@gmail.com>
2025-11-24 17:35:29 +1300

    Add full preprocessing pipeline file

---
+++ s07_full_pipeline.py
@@ -0,0 +1,92 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.cluster import KMeans
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import SimpleImputer
+from s01_load_data import load_housing_data, add_income_cat
+from s03_create_test_set import create_stratified_test_set
+from sklearn.pipeline import FunctionTransformer, make_pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.metrics.pairwise import rbf_kernel
+
+class ClusterSimilarity(BaseEstimator, TransformerMixin):
+    def __init__(self, n_clusters=10, gamma=1.0, random_state=None):
+        # No *args or **kwargs → keeps compatibility with get/set_params
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+
+    def fit(self, X, y=None, sample_weight=None):
+        # Create and fit the KMeans model
+        self.kmeans_ = KMeans(
+            n_clusters=self.n_clusters,
+            random_state=self.random_state
+        )
+        self.kmeans_.fit(X, sample_weight=sample_weight)
+        return self  # always return self
+
+    def transform(self, X):
+        # Compute Gaussian RBF similarity between samples and cluster centers
+        return rbf_kernel(
+            X,
+            self.kmeans_.cluster_centers_,
+            gamma=self.gamma
+        )
+
+    def get_feature_names_out(self, names=None):
+        # Nice readable feature names for pipelines / DataFrames
+        return [
+            f"Cluster {i} similarity"
+            for i in range(self.n_clusters)
+        ]
+        
+def column_ratio(X):
+    return X[:, [0]] / X[:, [1]]
+
+def ratio_name(function_transformer, feature_names_in):
+    return ["ratio"]
+
+def ratio_pipeline():
+    return make_pipeline(
+        SimpleImputer(strategy="median"),
+        FunctionTransformer(column_ratio, feature_names_out=ratio_name),
+        StandardScaler()
+    )
+    
+log_pipeline = make_pipeline(
+    SimpleImputer(strategy="median"),
+    FunctionTransformer(np.log, feature_names_out="one-to-one"),
+    StandardScaler()
+)
+
+cluster_simil = ClusterSimilarity(n_clusters=10, gamma=1., random_state=42)
+default_num_pipeline = make_pipeline(
+    SimpleImputer(strategy="median"),
+    StandardScaler()
+)
+
+cat_pipeline = make_pipeline(
+    SimpleImputer(strategy="most_frequent"),
+    OneHotEncoder(handle_unknown="ignore")
+)
+
+preprocessing = ColumnTransformer([
+    ("bedrooms", ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
+    ("rooms_per_house", ratio_pipeline(), ["total_rooms", "households"]),
+    ("people_per_house", ratio_pipeline(), ["population", "households"]),
+    ("log", log_pipeline, ["total_bedrooms", "total_rooms", "population",
+                           "households", "median_income"]),
+    ("geo", cluster_simil, ["latitude", "longitude"]),
+    ("cat", cat_pipeline, make_column_selector(dtype_include=object)),
+],
+    remainder=default_num_pipeline                            
+                                  )
+
+
+# strat_train_set, strat_test_set = create_stratified_test_set(load_housing_data())
+# housing = strat_train_set.drop("median_house_value", axis=1)
+# housing_labels = strat_train_set["median_house_value"].copy()
+
+# housing_prepared = preprocessing.fit_transform(housing)
+# print(housing_prepared.shape)
+# print(preprocessing.get_feature_names_out())
